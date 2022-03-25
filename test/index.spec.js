1b1cab94eca5b38158024131206cafda872ff3a1
Axelle-Ga <axelle.gaige@gmail.com>
2022-03-25 09:43:50 +0100

    Add (CI) : add test to CI

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,9 +5,10 @@ const app = require('../index')
 describe('index', function () {
     it('should return a valid response', function () {
         return request(app)
-            .get('/docs')
+            .get('/docs/')
             .expect(200)
             .then((res) => {
+                console.log(res);
                 // expect(res.body).to.have.property('version')
                 // expect(res.body.version).to.be.a.string
                 // expect(res.body.version).to.equal('1.0.0')
