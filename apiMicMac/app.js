dbacd30695b1150623c23c45413e899426c8212c
Axelle-Ga <axelle.gaige@gmail.com>
2022-03-28 11:53:52 +0200

    Fix (app) : fix merge

--- apiMicMac/app.js
+++ apiMicMac/app.js
@@ -4,10 +4,10 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
-
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const point2dRouter = require('./routes/point2d')
+const point3dRouter = require('./routes/point3d')
 
 const app = express()
 
@@ -21,10 +21,10 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/point2d', point2dRouter);
-app.use('/point3d', point3dRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/point2d', point2dRouter)
+app.use('/point3d', point3dRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -32,7 +32,7 @@ app.use(function (req, res, next) {
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
