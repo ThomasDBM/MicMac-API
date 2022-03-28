31d3366a73fe60c3bc0ff79b906b7fa28269d7b1
Axelle-Ga <axelle.gaige@gmail.com>
2022-03-28 11:10:32 +0200

    Refactor (CI) : adapt CI

---
+++ apiMicMac/test/index.spec.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+const request = require('supertest')
+const { expect } = require('chai')
+const app = require('../app')
+
+describe('app', function () {
+  it('should return a valid response', function () {
+    return request(app)
+      .get('/')
+      .expect(200)
+      .then((res) => {
+        // expect(res.body).to.have.property('version')
+        // expect(res.body.version).to.be.a.string
+        // expect(res.body.version).to.equal('1.0.0')
+      })
+      .catch((err) => expect(err).to.be.undefined)
+  })
+})
